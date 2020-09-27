cd301a6c0db57065bab5909e259d077890124853
Arut <mcginley@mail.ru>
2020-09-27 12:45:33 +0300

    Задание 4

--- src/task4.java
+++ src/task4.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class task4 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
         System.out.println("Введите число в бинарном формате: ");
-        String binary = sc.nextLine();
+        String binary = in.nextLine();
         int ten = Integer.parseInt(binary, 2);
         System.out.println("Число "+ binary + " в десятичном формате будет: " + ten);
     }
